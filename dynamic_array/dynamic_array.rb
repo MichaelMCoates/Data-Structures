d00c5146aa174a37538506f9ad4712ab34518fc5
Michael Coates <MichaelMCoates@gmail.com>
2017-10-09 18:17:22 -0400

    Change dynamic array internal start

--- dynamic_array/dynamic_array.rb
+++ dynamic_array/dynamic_array.rb
@@ -4,14 +4,24 @@ class DynamicArray
   attr_reader :length
 
   def initialize
-    self.static_array = StaticArray.new(4)
+    self.static_array = StaticArray.new(8)
     self.length = 0
-    self.capacity = 4
+    self.capacity = 8
+  end
+
+  def [](index)
+    self.static_array[index]
+  end
+
+  def []=(index, value)
+    self.static_array[index] = value
   end
 
 
+
   protected
-  attr_writer :static_array, :length, :capacity
+  attr_accessor :static_array, :capacity
+  attr_writer :length
 
 
 
