625028a88c64b2de69121544a277f66aa872a96c
Michael Coates <MichaelMCoates@gmail.com>
2017-10-09 18:58:23 -0400

    Change static_array to store

--- dynamic_array/dynamic_array.rb
+++ dynamic_array/dynamic_array.rb
@@ -4,19 +4,19 @@ class DynamicArray
   attr_reader :length
 
   def initialize
-    self.static_array = StaticArray.new(8)
+    self.store = StaticArray.new(8)
     self.length = 0
     self.capacity = 8
   end
 
   def [](index)
     check_index(index)
-    self.static_array[index]
+    self.store[index]
   end
 
   def []=(index, value)
     check_index(index)
-    self.static_array[index] = value
+    self.store[index] = value
   end
 
   def push(value)
@@ -47,7 +47,7 @@ class DynamicArray
 
 
   protected
-  attr_accessor :static_array, :capacity
+  attr_accessor :store, :capacity
   attr_writer :length
 
   def check_index(index)
