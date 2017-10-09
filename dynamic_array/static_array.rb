e2e208ac18774de33b79baf3cefc55b2c4c3e701
Michael Coates <MichaelMCoates@gmail.com>
2017-10-09 17:52:27 -0400

    Set up Static Array class

--- dynamic_array/static_array.rb
+++ dynamic_array/static_array.rb
@@ -1,3 +1,17 @@
 class StaticArray
+  def initialize(length)
+    @store = Array.new(length)
+  end
+
+  def [](index)
+    @store[index]
+  end
+
+  def []=(index, value)
+    @store[index] = value
+  end
+
+  protected
+  attr_accessor :store
 
 end
