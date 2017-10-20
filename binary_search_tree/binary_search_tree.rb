c602a49bcdd24a00fc07ea6449f6a740fb680f22
Michael Coates <MichaelMCoates@gmail.com>
2017-10-19 20:47:54 -0400

    Add BST insert, min, max

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -9,7 +9,54 @@ class TreeNode
 end
 
 class BinarySearchTree
-  def initialize()
+  def initialize
+    @root = nil
   end
+
+  def insert(value)
+    if !@root
+      @root = TreeNode.new(value)
+      return
+    end
+
+    BinarySearchTree.insert!(@root, value)
+  end
+
+  def self.insert!(node, value)
+    return TreeNode.new(value) unless node
+
+    if value <= node.value
+      node.left = BinarySearchTree.insert!(node.left, value)
+    else
+      node.right = BinarySearchTree.insert!(node.right, value)
+    end
+
+    node
+  end
+
+  def min
+    BinarySearchTree.min(@root)
+  end
+
+  def self.min(node)
+    return nil unless node
+    return node unless node.left
+
+    BinarySearchTree.min(node.left)
+  end
+
+  def max
+    BinarySearchTree.max(@root)
+  end
+
+  def self.max(node)
+    return nil unless node
+    return node unless node.right
+
+    BinarySearchTree.max(node.right)
+  end
+
+
+
 
 end
